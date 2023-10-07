49acbfc685a6dce44e0e3300f1141f948c658152
suleo <dashu975@163.com>
2023-10-07 16:29:50 +0800

    update leak_scan

--- cmd/leak_scan.py
+++ cmd/leak_scan.py
@@ -36,8 +36,8 @@ def single_scan(url, proxy):
                        attrs=["bold", "reverse"])
                 vulnerable_paths.append(target)
             else:
-                pass
-                # cprint(f"[-] 状态码{response.status_code} 无法访问 URL为:{target}", "yellow")
+                # pass
+                cprint(f"[-] 状态码{response.status_code} 无法访问 URL为:{target}", "yellow")
         except Exception as e:
             cprint(f"[-] {e} 无法访问 URL为:{target}", "yellow")
 
